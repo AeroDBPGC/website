41610f7d6e1a9fc2d71dfd8ed4e289154cab8484
Varun Bankar <mail.varunbankar@gmail.com>
2020-05-01 13:33:32 +0530

    organise project card page

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -1,39 +1,39 @@
 import React from "react"
 
+// For Bootstrap responsive layout.
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 
-
-
+// Styles for Projects page.
 import projectsPageStyle from "../styles/projects/projects.module.scss"
 
+// Custom components for Projects page.
 import Header from "../components/header"
 import ProjectCard from "../components/projects/projectcard"
 
+// Project page.
 const ProjectPage = () => (
-    <>
-        <div className={projectsPageStyle.wrapper} style={{ justifyContent:`start` }}>
-            <Header siteTitle="PROJECTS" />
-            <Container className={projectsPageStyle.c1}>
-                <Row>
-                    <Col>
-                        <ProjectCard></ProjectCard>
-                    </Col>
-                </Row>
-                <Row>
-                    <Col>
-                        <ProjectCard></ProjectCard>
-                    </Col>
-                </Row>
-                <Row>
-                    <Col>
-                        <ProjectCard></ProjectCard>
-                    </Col>
-                </Row>
-            </Container>
-        </div>
-    </>
+    <div className={projectsPageStyle.wrapper}>
+        <Header siteTitle="PROJECTS" />
+        <Container className={projectsPageStyle.container}>
+            <Row>
+                <Col>
+                    <ProjectCard title={"PROJECT NAME"} description={"This is a brief description."} />
+                </Col>
+            </Row>
+            <Row>
+                <Col>
+                    <ProjectCard title={"FOREST DRONE"} description={"Best forest drone in town."} />
+                </Col>
+            </Row>
+            <Row>
+                <Col>
+                    <ProjectCard title={"RC AIRPLANE"} description={"Fly like never before."} />
+                </Col>
+            </Row>
+        </Container>
+    </div>
 )
 
 export default ProjectPage
