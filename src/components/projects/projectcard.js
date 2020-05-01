b65d11cb42ad9f5c1a60683b36348bb5419c0379
Varun Bankar <mail.varunbankar@gmail.com>
2020-05-02 00:25:59 +0530

    refactor and complete profile page

--- src/components/projects/projectcard.js
+++ src/components/projects/projectcard.js
@@ -5,12 +5,12 @@ import projectCardStyle from "../../styles/projects/projectcard.module.scss"
 
 // Project Card component.
 const ProjectCard = ({ title, description }) => (
-    <div className={projectCardStyle.card}>
-        <div className={projectCardStyle.textWrapper}>
-            <h1 className={projectCardStyle.title}>{ title }</h1>
-            <p className={projectCardStyle.description}>{ description }</p>
-        </div>
+  <div className={projectCardStyle.card}>
+    <div className={projectCardStyle.textWrapper}>
+      <h1 className={projectCardStyle.title}>{ title }</h1>
+      <p className={projectCardStyle.description}>{ description }</p>
     </div>
+  </div>
 )
 
 export default ProjectCard
