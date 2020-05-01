24077fdd57924889a848081ba2c8ce5103be4b30
Varun Bankar <mail.varunbankar@gmail.com>
2020-05-01 13:33:17 +0530

    improve project card design

--- src/components/projects/projectcard.js
+++ src/components/projects/projectcard.js
@@ -1,13 +1,14 @@
-
 import React from "react"
 
+// Styles for Project Card component.
 import projectCardStyle from "../../styles/projects/projectcard.module.scss"
 
-const ProjectCard = () => (
+// Project Card component.
+const ProjectCard = ({ title, description }) => (
     <div className={projectCardStyle.card}>
-        <div className={projectCardStyle.image}>
-            <h1 className={projectCardStyle.title}>PROJECT NAME</h1>
-            <p className={projectCardStyle.description}>This is a brief project description.</p>
+        <div className={projectCardStyle.textWrapper}>
+            <h1 className={projectCardStyle.title}>{ title }</h1>
+            <p className={projectCardStyle.description}>{ description }</p>
         </div>
     </div>
 )
