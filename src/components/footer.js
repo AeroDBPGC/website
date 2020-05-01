ae616442a927744f0b9c287972ccf989d621ee1f
Varun Bankar <mail.varunbankar@gmail.com>
2020-05-01 14:15:54 +0530

    create footer component

---
+++ src/components/footer.js
@@ -0,0 +1,15 @@
+import React from "react"
+
+const style = {
+  textAlign: `center`,
+  lineHeight: `1`,
+  fontSize: `1rem`,
+}
+
+const Footer = () => (
+  <footer  style={{height: `50px`, padding: `1rem`}}>
+    <h5  style={style}>AERODYNAMICS CLUB © 2020 // VARUN BANKAR</h5>
+  </footer>
+)
+
+export default Footer
