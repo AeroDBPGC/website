0ef8359acd956811be3e5f269685e96354197c7b
Varun Bankar <mail.varunbankar@gmail.com>
2020-05-01 13:32:54 +0530

    update contact.js

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -11,7 +11,6 @@ import ContactCard from "../components/contact/contactcards"
 import Header from "../components/header"
 
 const ContactPage = () => (
-  <>
     <div className={contactPageStyle.wrapper}>
         <Header siteTitle="CONTACT US" />
         <Container fluid="true" className={contactPageStyle.c2}>
@@ -22,21 +21,20 @@ const ContactPage = () => (
             </Row>
             <Row noGutters="true" style={{ padding:`.5rem`, display:`flex`, flexGrow:`1` }}>
                 <Col lg={6} xl={3} md={6} xs={12}>
-                    <ContactCard background="linear-gradient(45deg, #606c88, #3f4c6b)"></ContactCard>
+                    <ContactCard background={`linear-gradient(45deg, #606c88, #3f4c6b)`}/>
                 </Col>
                 <Col lg={6} xl={3} md={6} xs={12}>
-                    <ContactCard background="linear-gradient(45deg, #e13680, #a43ab2)"></ContactCard>
+                    <ContactCard background={"linear-gradient(45deg, #e13680, #a43ab2)"}/>
                 </Col>
                 <Col lg={6} xl={3} md={6} xs={12}>
-                    <ContactCard background="linear-gradient(220deg, #F7971E, #FFD200)"></ContactCard>
+                    <ContactCard background={"linear-gradient(220deg, #F7971E, #FFD200)"}/>
                 </Col>
                 <Col lg={6} xl={3} md={6} xs={12}>
-                    <ContactCard background="linear-gradient(45deg, #41d8dd, #5583ee)"></ContactCard>
+                    <ContactCard background={"linear-gradient(45deg, #41d8dd, #5583ee)"}/>
                 </Col>
             </Row>
         </Container>
     </div>
-    </>
 )
 
 export default ContactPage
