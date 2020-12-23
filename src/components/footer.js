ce4f1dc00c3e64a2fb7469702fb4851977ea8f1e
Dev Goel <goeldev01@gmail.com>
2020-12-23 14:21:45 +0530

    Centered social media icons, fixed footer design

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,5 +1,5 @@
 import React from "react"
-import { FaLinkedin, FaGithub, FaFacebook, FaInstagram, FaWikipediaW, FaEnvelope,FaMedium } from 'react-icons/fa';
+import { FaLinkedin, FaGithub, FaFacebook, FaInstagram, FaWikipediaW, FaEnvelope, FaMedium } from 'react-icons/fa';
 import footerCardStyle from "../styles/footer/footer.module.scss"
 
 const style = {
@@ -9,21 +9,18 @@ const style = {
 }
 
 const Footer = () => (
-  <footer  style={{padding: `0.5rem`}}>
-    <h5  style={style}>AERODYNAMICS CLUB © 2020-2021</h5>
+  <footer style={{ padding: `0.5rem` }}>
     <div className={footerCardStyle.socialIconContainer}>
-      <div className={footerCardStyle.socialIconWrapper}></div>
-        <div className={footerCardStyle.icon}>
-          <a href={'https://github.com/orgs/Aerodynamics-Club/'}><FaGithub color="#000"/></a>
-          <a href={'https://www.facebook.com/AeroD.BITSGoa/'}><FaFacebook color="#000"/></a>
-          <a href={'mailto: ic.aero@goa.bits-pilani.ac.in'}><FaEnvelope color="#000"/></a>
-          <a href={'https://www.bitslite.in/wiki/Clubs/Aerodynamics_Club'}><FaWikipediaW color="#000"/></a>
-          <a href={'https://medium.com/@ic.aero'}><FaMedium color="#000"/></a>
-
-        </div>
-    </div>
-    
-  </footer>
+      <div className={footerCardStyle.socialIconWrapper}>
+        <div className={footerCardStyle.icon}><a href={'https://github.com/orgs/Aerodynamics-Club/'}><FaGithub color="#000" /></a></div>
+        <div className={footerCardStyle.icon}><a href={'https://www.facebook.com/AeroD.BITSGoa/'}><FaFacebook color="#000" /></a></div>
+        <div className={footerCardStyle.icon}><a href={'mailto: ic.aero@goa.bits-pilani.ac.in'}><FaEnvelope color="#000" /></a></div>
+        <div className={footerCardStyle.icon}><a href={'https://www.bitslite.in/wiki/Clubs/Aerodynamics_Club'}><FaWikipediaW color="#000" /></a></div>
+        <div className={footerCardStyle.icon}><a href={'https://medium.com/@ic.aero'}><FaMedium color="#000" /></a></div>
+      </div >
+    </div >
+    <h5 style={style}>AERODYNAMICS CLUB © 2020-2021</h5>
+  </footer >
 )
 
 export default Footer
