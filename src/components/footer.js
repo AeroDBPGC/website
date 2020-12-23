703cc5ae3d4fa176f5878fdc5a4f114694147d1c
Dev Goel <goeldev01@gmail.com>
2020-12-23 14:28:39 +0530

    Minor changes to Footer component

--- src/components/footer.js
+++ src/components/footer.js
@@ -12,11 +12,11 @@ const Footer = () => (
   <footer style={{ padding: `0.5rem` }}>
     <div className={footerCardStyle.socialIconContainer}>
       <div className={footerCardStyle.socialIconWrapper}>
-        <div className={footerCardStyle.icon}><a href={'https://github.com/orgs/Aerodynamics-Club/'}><FaGithub color="#000" /></a></div>
-        <div className={footerCardStyle.icon}><a href={'https://www.facebook.com/AeroD.BITSGoa/'}><FaFacebook color="#000" /></a></div>
-        <div className={footerCardStyle.icon}><a href={'mailto: ic.aero@goa.bits-pilani.ac.in'}><FaEnvelope color="#000" /></a></div>
-        <div className={footerCardStyle.icon}><a href={'https://www.bitslite.in/wiki/Clubs/Aerodynamics_Club'}><FaWikipediaW color="#000" /></a></div>
-        <div className={footerCardStyle.icon}><a href={'https://medium.com/@ic.aero'}><FaMedium color="#000" /></a></div>
+        <a className={footerCardStyle.icon} href={'https://github.com/orgs/Aerodynamics-Club/'}><FaGithub color="#000" /></a>
+        <a className={footerCardStyle.icon} href={'https://www.facebook.com/AeroD.BITSGoa/'}><FaFacebook color="#000" /></a>
+        <a className={footerCardStyle.icon} href={'mailto: ic.aero@goa.bits-pilani.ac.in'}><FaEnvelope color="#000" /></a>
+        <a className={footerCardStyle.icon} href={'https://www.bitslite.in/wiki/Clubs/Aerodynamics_Club'}><FaWikipediaW color="#000" /></a>
+        <a className={footerCardStyle.icon} href={'https://medium.com/@ic.aero'}><FaMedium color="#000" /></a>
       </div >
     </div >
     <h5 style={style}>AERODYNAMICS CLUB © 2020-2021</h5>
